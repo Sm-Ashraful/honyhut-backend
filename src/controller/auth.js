05bc0a41985425cb8be21e62ed5f86102090a393
mitali-international-limited <124981715+mitali-international-limited@users.noreply.github.com>
2023-05-04 16:50:46 +0600

    initial commit

---
+++ src/controller/auth.js
@@ -0,0 +1,75 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const shortid = require("shortid");
+exports.signup = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      return res.status(400).json({
+        message: "User already registered",
+      });
+    }
+
+    const { name, email, password } = req.body;
+    const hash_password = await bcrypt.hash(password, 10);
+
+    const _user = new User({
+      name,
+      email,
+      hash_password,
+      username: shortid.generate(),
+    });
+
+    const savedUser = await _user.save();
+    if (savedUser) {
+      return res.status(201).json({
+        message: "Sign Up successfully...!",
+      });
+    }
+  } catch (error) {
+    return res.status(400).json({
+      message: "Something Went Wrong",
+    });
+  }
+};
+exports.signin = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid User Name or Password" });
+    }
+
+    const isPasswordValid = await user.authenticate(req.body.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid User Name or Password" });
+    }
+
+    const token = jwt.sign(
+      { _id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "48h",
+      }
+    );
+
+    const { _id, name, email, role } = user;
+
+    res.cookie("token", token, { expiresIn: "48h" });
+
+    return res.status(200).json({
+      token,
+      user: {
+        _id,
+        name,
+        email,
+        role,
+      },
+    });
+  } catch (error) {
+    return res.status(400).json({ message: "Something Went Wrong" });
+  }
+};
---
+++ src/index.server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const env = require("dotenv");
+const { response } = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const bodyParser = require("body-parser");
+port = 3080;
+
+// routes
+const authRoutes = require("./routes/auth");
+
+//mongo db connection
+const url =
+  "mongodb+srv://dev1mitaliint:8k9xEQweFVA565sm@cluster0.osjggo2.mongodb.net/?retryWrites=true&w=majority";
+async function connect() {
+  try {
+    await mongoose.connect(url);
+    console.log("MongoDb connected");
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+}
+connect();
+// environment variable
+env.config();
+
+app.use(bodyParser.json());
+app.use("/api", authRoutes);
+app.get("/", (req, res) => {
+  res.send("App Works !!!!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on the port::${port}`);
+});
